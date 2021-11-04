2301aec58ab1f3c5a86addfa76308ab6c148cf66
Tomeh-8 <tomeh888@gmail.com>
2021-11-04 15:46:32 +0100

    mongo url edit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 //mongo set up
-mongoose.connect("mongodb://localhost/picSho", { useNewUrlParser: true, useUnifiedTopology: true },
+mongoose.connect( process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
     (err) => {
        if (err) throw err;
        console.log("connected to dB!");
@@ -24,6 +24,10 @@ app.use(cors());
 //routes
  app.use("/", postRoute);
  app.use("/users", userRoute);
+
+ app.get("/", (req, res) => {
+    res.send("Welcome to picMood Api!")
+ });
 
 
 //listen
